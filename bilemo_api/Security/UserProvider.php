0695dcb0803799a9b1c75982e23b646f2b102cd4
Thomas75019 <tlarousse3@gmail.com>
2019-10-10 18:20:31 +0400

    Make UserProvider

---
+++ bilemo_api/Security/UserProvider.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Security;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use App\Entity\Client;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+class UserProvider implements UserProviderInterface
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * UserProvider constructor.
+     * @param EntityManagerInterface $manager
+     */
+    public function __construct(EntityManagerInterface $manager)
+    {
+        $this->entityManager = $manager;
+    }
+
+    /**
+     * @param string $name
+     * @return mixed|UserInterface
+     * @throws \Doctrine\ORM\NonUniqueResultException
+     */
+    public function loadUserByUsername($name)
+    {
+        return $this->entityManager->createQueryBuilder()
+            ->select('c')
+            ->from('Client', 'c')
+            ->where('c.name = :name')
+            ->setParameter('name', $name)
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+
+    /**
+     * @param UserInterface $user
+     * @return UserInterface
+     */
+    public function refreshUser(UserInterface $user)
+    {
+        if (!$user instanceof Client) {
+            throw new UnsupportedUserException(
+                sprintf('Instances of "%s" are not supported.', get_class($user))
+            );
+        }
+        return $user;
+    }
+
+    /**
+     * @param string $class
+     * @return bool
+     */
+    public function supportsClass($class)
+    {
+        return $class === 'App\Security\User';
+    }
+
+
+}
