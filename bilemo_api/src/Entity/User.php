1bba692ccd60bbcfe10d0c3ba60fa7801ffa46b5
Thomas75019 <tlarousse3@gmail.com>
2019-10-24 16:27:51 +0400

    Delete useless hydrate function on User entity

--- bilemo_api/src/Entity/User.php
+++ bilemo_api/src/Entity/User.php
@@ -152,16 +152,4 @@ class User
     {
         $this->client = $client;
     }
-
-    public function hydrate($data)
-    {
-        foreach ($data as $key => $value) {
-            $method = 'set'.ucfirst($key);
-            if (method_exists($this, $method)) {
-                $this->$method($value);
-            }
-        }
-    }
-
-
 }
