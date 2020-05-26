b3e1628ebe1025830ab0df16dd9c03645bc60bd3
Thomas75019 <tlarousse3@gmail.com>
2020-05-26 15:35:57 +0200

    Add ServerError listener who handles error 500

---
+++ bilemo_api/src/EventListener/ServerErrorListener.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+
+class ServerErrorListener
+{
+    public function onKernelException(ExceptionEvent $event)
+    {
+
+        $response = new Response();
+
+        $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        $response->setContent(json_encode([
+            500 => 'Internal Server Error'
+        ]));
+
+
+        $event->setResponse($response);
+
+        return $response;
+    }
+}
